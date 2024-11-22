51924128117061b34838d43cf4dfd7b2f86ed597
shahidfarooq231 <shahidfarooq.cs@gmail.com>
2024-11-22 05:34:53 -0800

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -3,12 +3,12 @@ print("2 -for subtraction")
 print("3 -for multiply")
 print("4 -for division")
 
-option = int(input("choose an operation: "))
+option = float(input("choose an operation: "))
 
 if(option  in [1,2,3,4]):
    
-   num1 = int(input("enter first number: "))
-   num2 = int(input("enter second number: "))
+   num1 = float(input("enter first number: "))
+   num2 = float(input("enter second number: "))
 
 if(option == 1):
    result = num1 + num2
@@ -17,7 +17,7 @@ elif(option == 2):
 elif(option == 3):
    result = num1 * num2
 elif(option == 4):
-   result = num1 // num2
+   result = num1 / num2
 
 else:
     print("invalid operation ")
